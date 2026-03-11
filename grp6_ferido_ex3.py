71e1520c8d6023182fd27ff5a8a47dac448257f8
Christian Charlz Ferido <christiancharlz01.gf@gmail.com>
2026-03-11 09:17:42 +0800

    Add files via upload

---
+++ grp6_ferido_ex3.py
@@ -0,0 +1,24 @@
+
+#Python Comments
+#My first simple script
+print("Hello, I'm Christian Charlz Ferido! BSIT-1V")
+
+#Python Variables
+name = "Charlz"
+number = 18
+
+print(name)
+print(number)
+
+#Python - Variable Names
+marc = "Marc"
+jeshley = "Jeshley"
+cedric = "Cedric"
+jerome = "Jerome"
+
+#Many Values to Multiple Variables
+j, c, f = 1,2,3
+
+#One Values to Multiple Variables
+j= c= f= 1
+
