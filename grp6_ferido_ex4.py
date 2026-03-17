85dafa35758304df48f7b2a185d114800d0def1c
Christian Charlz Ferido <christiancharlz01.gf@gmail.com>
2026-03-17 21:27:30 +0800

    Implement arithmetic operations and type checks
    Added operations for integers, floats, and complex numbers, along with examples of integer representation and type checking.

--- grp6_ferido_ex4.py
+++ grp6_ferido_ex4.py
@@ -1,1 +1,50 @@
+#Laboratory Exercise 4
+#MDAS
 
+# Integer values
+int1 = 15
+int2 = 5
+print("INTEGER OPERATIONS")
+print("Addition:", int1 + int2)
+print("Subtraction:", int1 - int2)
+print("Multiplication:", int1 * int2)
+print("Division:", int1 / int2)
+print("Modulus:", int1 % int2)
+print("Exponent:", int1 ** int2)
+
+# Float values
+float1 = 20.5
+float2 = 3.5
+print("FLOAT OPERATIONS")
+print("Addition:", float1 + float2)
+print("Subtraction:", float1 - float2)
+print("Multiplication:", float1 * float2)
+print("Division:", float1 / float2)
+print("Modulus:", float1 % float2)
+print("Exponent:", float1 ** float2)
+
+# Complex values
+comp1 = 4 + 3j
+comp2 = 3 + 2j
+print("COMPLEX OPERATIONS")
+print("Addition:", comp1 + comp2)
+print("Subtraction:", comp1 - comp2)
+print("Multiplication:", comp1 * comp2)
+print("Division:", comp1 / comp2)
+print("Exponent:", comp1 ** 2)
+
+#2 Integer with and without underscores
+num1 = 25000000
+num2 = 25_000_000
+
+print(num1)
+print(num2)
+
+#3 Check data types
+a = 10          
+b = 5.5         
+c = 2 + 6j    
+
+print(type(a))
+print(type(b))
+print(type(c))
